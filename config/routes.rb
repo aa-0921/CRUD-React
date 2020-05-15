651b08189dd701cda8904c5c8bc5385f2daf63d0
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-15 17:30:49 +0900

    routes.rb編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'react_crud_data#index'
+
+  get 'react_crud_data/index'
+  get 'react_crud_data/new',   to: 'react_crud_data#new',
+                               as: 'new_react_crud_data'
+  post 'react_crud_data',      to: 'react_crud_data#create'
+  get 'react_crud_data/:id',   to: 'react_crud_data#update'
+  get 'react_crud_data/:id',   to: 'react_crud_data#destroy'
 end
